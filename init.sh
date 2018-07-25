397d9b250e492602c0aaab4df38344e4034bc494
ccmlulu <ccmlulu@ccmlulunoMacBook-puro.local>
2018-07-25 23:47:42 +0900

    update

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 /usr/local/bin/brew bundle
 
--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -3,4 +3,5 @@
 DOTFILES_CWD=`pwd`
 ln -nfs $DOTFILES_CWD/dot.tmux.conf ${HOME}/.tmux.conf
 ln -nfs $DOTFILES_CWD/dot.zshrc ${HOME}/.zshrc
+ln -nfs $DOTFILES_CWD/dot.vimrc ${HOME}/.vimrc
 
